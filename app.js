f052abc318679aefe5bedf0a9d005a464977e41b
DjAbiyev <djamil@partnersdentalstudio.com>
2026-03-19 13:56:46 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+const inventory = [
+  { name: 'Hornitos Plata', size: '750ML', category: 'Liquor', supplierName: 'Southern Wine & Spirits' },
+  { name: 'The Prisoner Red Blend', size: '750ML', category: 'Wine', supplierName: 'Southern Wine & Spirits' },
